bfc551e362503b8111523b5280ee3a4e41ad1bca
Philipp Jenke <philipp.jenke@haw-hamburg.de>
2022-01-20 16:03:44 +0100

    Replaced copyright notice.

--- src/main/java/wpcg/lab/a5/A5Scene.java
+++ src/main/java/wpcg/lab/a5/A5Scene.java
@@ -1,6 +1,6 @@
 /**
- * Diese Datei ist Teil des Vorgabeframeworks zur Lehrveranstaltung Einführung in die Computergrafik
- * an der Hochschule für Angewandte Wissenschaften Hamburg von Prof. Philipp Jenke (Informatik)
+ * This file is part of the computer graphics project of the computer graphics group led by
+ * Prof. Dr. Philipp Jenke at the University of Applied Sciences (HAW) in Hamburg.
  */
 
 package wpcg.lab.a5;
