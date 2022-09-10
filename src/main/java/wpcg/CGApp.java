9def9a356a0c19e8b6465bb474e677ed585175ca
abo781 <philipp.jenke@haw-hamburg.de>
2022-09-10 11:35:21 +0200

    Fixed app class with correct scene.

--- src/main/java/wpcg/CGApp.java
+++ src/main/java/wpcg/CGApp.java
@@ -7,12 +7,7 @@ package wpcg;
 
 import com.jme3.system.AppSettings;
 import ui.CG3DApplication;
-import wpcg.lab.a4.A4Scene;
-import wpcg.lab.a7.A7Scene;
-import wpcg.lab.solution.a4.A4SceneSolution;
-import wpcg.lab.solution.a5.A5SceneSolution;
-import wpcg.lab.solution.a6.A6SceneSolution;
-import wpcg.lab.solution.a7.A7SceneSolution;
+import wpcg.lab.a1.IntroScene;
 
 /**
  * This is the base class for the lecture exercise in "WP Computergrafik"
@@ -21,7 +16,7 @@ public class CGApp extends CG3DApplication {
 
     public CGApp() {
         // Set the 3D scene (replace for each assignment)
-        setScene3D(new A7SceneSolution());
+        setScene3D(new IntroScene());
     }
 
     public static void main(String[] args) {
